3db26a263cff9063e545c7af7533a0f1b657adde
Anthony Simmons <AnthonySimmons99@gmail.com>
2018-09-02 14:30:19 -0700

    Add files

---
+++ NUnitTearDownBug/Adder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTearDownBug
+{
+    public class Adder : IDisposable
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public void Dispose()
+        {
+            //Example
+        }
+    }
+}
---
+++ Tests/NUnitTearDownBug.Test/NUnitTearDownBugTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnitTearDownBug.Test
+{
+    [TestFixture]
+    public class NUnitTearDownBugTest : IDisposable
+    {
+        private Adder mSut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mSut = new Adder();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mSut = null;
+        }
+
+        public void Dispose()
+        {
+            //This will throw.
+            mSut.Dispose();
+        }
+
+        [TestCase(3, 5, 8)]
+        public void Add_ShouldReturnExpectedValue(int x, int y, int expectedValue)
+        {
+            Assert.AreEqual(expectedValue, mSut.Add(x, y));
+        }
+    }
+}
